5eb57dde56b4c941fc6a9daef41a15cffc01da4f
ayodejiAA <afolabiayodejia@gmail.com>
2019-10-08 14:44:24 +0100

    ch(setup): Setup React Project - Add homepage folder - Webpack - Eslint - Babel

---
+++ src/components/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import HomePage from './HomePage/HomePage';
+
+const App = () => <HomePage />;
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './assets/images/favicon.ico';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root);
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept();
+}
